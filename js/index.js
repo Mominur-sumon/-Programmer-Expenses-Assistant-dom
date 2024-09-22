099654e14fed39ad690367f0cba4e8954b9c2d86
Md Mominur Rahman <mominur.sumon1@gmail.com>
2024-09-23 02:00:44 +0600

    history section added

--- js/index.js
+++ js/index.js
@@ -20,10 +20,23 @@ calculateBtn.addEventListener("click", function () {
     totalExpenseElement.innerText = totalExpense.toFixed(2);
 
     const balance = document.getElementById("balance");
-    balance.innerText = (income - totalExpense).toFixed(2);
+    const newBalance = income - totalExpense;
+    balance.innerText = newBalance.toFixed(2);
 
     const result = document.getElementById("results");
     result.classList.remove("hidden");
+
+
+    const historyItem = document.createElement("div");
+    historyItem.classList.add( "border-b", "rounded-md", "border-gray-300", "py-2");
+    historyItem.innerHTML = `
+    <p class="text-lg font-semibold">${new Date().toLocaleDateString()}</p>
+    <p class="text-lg font-semibold">Income:  ${income}</p>
+    <p class="text-lg font-semibold">Expense:  ${totalExpense}</p>
+    <p class="text-lg font-semibold"> Balance:  ${newBalance}</p>
+    `
+    const historyList = document.getElementById("history-list");
+    historyList.insertBefore(historyItem, historyList.firstChild);
 });
 
 const calculateSavingsBtnEl = document.getElementById("calculate-savings");
@@ -43,5 +56,27 @@ calculateSavingsBtnEl.addEventListener("click", function () {
 
     const remainigBalance = document.getElementById("remaining-balance");
     remainigBalance.innerText = (balance - savings).toFixed(2);
+
+});
+
+
+// history tab functionality
+
+const historyTabBtn = document.getElementById("history-tab");
+const assistantTabBtn = document.getElementById("assistant-tab");
+historyTabBtn.addEventListener("click", function () {
+    historyTabBtn.classList.add("text-white", "font-semibold", "bg-gradient-to-r", "from-blue-500", "to-purple-600");
+
+    assistantTabBtn.classList.remove("text-white", "font-semibold", "bg-gradient-to-r", "from-blue-500", "to-purple-600");
+
+    document.getElementById("expense-form").classList.add("hidden");
+    document.getElementById("history-section").classList.remove("hidden");
+
+});
+
+assistantTabBtn.addEventListener("click", function () {
+    assistantTabBtn.classList.add("text-white", "font-semibold", "bg-gradient-to-r", "from-blue-500", "to-purple-600");
+
+    historyTabBtn.classList.remove("text-white", "font-semibold", "bg-gradient-to-r", "from-blue-500", "to-purple-600");
 
 });
